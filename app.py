906a0d37cbc7bd7e2aad68bd489ac303f1f35992
Ailin Liu <ailinliu.al@gmail.com>
2019-01-05 17:14:43 -0500

    Demo

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def convert_date(val):
 
 
 if __name__ == '__main__':
-    port=int(os.envron.get("PORT",5000))
+    port=int(os.environ.get("PORT",5000))
     app.run(host='0.0.0.0', port=port)
 #    app.run(port=33507)
 #    app.run(debug=True)
